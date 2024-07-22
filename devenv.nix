fbf779492b80b2cbf84c667d46d5bb7cad676ecc
start.vaadin.com <no-reply@vaadin.com>
2024-07-22 17:21:23 +0000

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ devenv.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }: {
+  packages = [ pkgs.git ];
+
+  languages = {
+    java = {
+      enable = true;
+      jdk.package = pkgs.jdk21;
+      maven.enable = true;
+      maven.package = pkgs.maven;
+    };
+    shell.enable = true;
+    javascript.enable = true;
+    typescript.enable = true;
+    nix.enable = true;
+  };
+
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql;
+    listen_addresses = "127.0.0.1";
+    initialDatabases = [{ name = "shsponsors_db"; }];
+  };
+
+  devcontainer.enable = true;
+  difftastic.enable = true;
+  dotenv.enable = true;
+
+  pre-commit.hooks = {
+    actionlint.enable = true;
+    commitizen.enable = true;
+    markdownlint.enable = true;
+  };
+}
---
+++ src/main/java/com/example/application/views/helloflow/HelloFlowView.java
@@ -0,0 +1,38 @@
+package com.example.application.views.helloflow;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import jakarta.annotation.security.PermitAll;
+
+@PageTitle("Hello Flow")
+@Menu(icon = "line-awesome/svg/globe-solid.svg", order = 0)
+@Route(value = "")
+@RouteAlias(value = "")
+@PermitAll
+public class HelloFlowView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public HelloFlowView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
